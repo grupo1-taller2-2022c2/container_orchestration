4890929c9bf2336004e985ab032b4635a49e3429
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-10-09 17:00:08 -0300

    fixes and more

---
+++ log_containers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. constants.env
+
+docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_FRONT_SERVICE_NAME > logs/backoffice_front_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $API_GATEWAY_SERVICE_NAME > logs/api_gateway_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_API_GATEWAY_SERVICE_NAME > logs/backoffice_api_gateway_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $USERS_MS_SERVICE_NAME > logs/users_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $USERS_DB_SERVICE_NAME > logs/users_database_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $TRIPS_MS_SERVICE_NAME > logs/trips_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $TRIPS_DB_SERVICE_NAME > logs/trips_database_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_MS_SERVICE_NAME > logs/backoffice_logs.txt &
+docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_DB_SERVICE_NAME > logs/backoffice_database_logs.txt &
--- start_dev.sh
+++ start_dev.sh
@@ -26,21 +26,10 @@ docker-compose up -d $USERS_MS_SERVICE_NAME
 docker-compose up -d $TRIPS_MS_SERVICE_NAME
 docker-compose up -d $BACKOFFICE_MS_SERVICE_NAME
 
-sleep 3
+sleep 5
 
 # Logging in files
-mkdir logs &
-docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_FRONT_SERVICE_NAME > logs/backoffice_front_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $API_GATEWAY_SERVICE_NAME > logs/api_gateway_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_API_GATEWAY_SERVICE_NAME > logs/backoffice_api_gateway_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $USERS_MS_SERVICE_NAME > logs/users_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $USERS_DB_SERVICE_NAME > logs/users_database_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $TRIPS_MS_SERVICE_NAME > logs/trips_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $TRIPS_DB_SERVICE_NAME > logs/trips_database_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_MS_SERVICE_NAME > logs/backoffice_logs.txt &
-docker-compose logs -f --no-color --tail=1000 $BACKOFFICE_DB_SERVICE_NAME > logs/backoffice_database_logs.txt &
-
-sleep 5
+mkdir logs
 
 echo "Applying migrations to users database..."
 docker-compose exec $USERS_MS_SERVICE_NAME alembic upgrade head || echo "Wait for image to build and run ./start_dev.sh again..."
@@ -53,3 +42,5 @@ docker-compose exec $BACKOFFICE_MS_SERVICE_NAME alembic upgrade head || echo "Wa
 
 echo ""
 echo "In case you want to enter inside any docker with bash, run: docker-compose exec _service_name_ /bin/bash"
+
+./log_containers.sh
