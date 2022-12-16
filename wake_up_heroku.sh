4a9911981cf35d9af9dbf262bda2f4129a49cda9
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-12-15 23:32:49 -0300

    wake up heroku  script added

---
+++ wake_up_heroku.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl https://fi-uber-users.herokuapp.com &
+curl https://fi-uber-trips.herokuapp.com &
+curl https://fi-uber-wallets.herokuapp.com &
+curl https://fi-uber-backoffice.herokuapp.com &
+curl https://fi-uber-api-gateway.herokuapp.com &
+curl https://fi-uber-api-gateway-backoffice.herokuapp.com &
+curl https://fi-uber-front-backoffice.herokuapp.com &
